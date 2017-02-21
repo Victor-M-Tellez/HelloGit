b781e2f0d3fcc5a6554bcd7a821af1256f5cb9f8
Bryant Gonzaga <Bryant Gonzaga>
2017-02-21 15:11:46 -0500

    It says hello

---
+++ src/HelloGit.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Bryant Gonzaga on 2/21/2017.
+ */
+public class HelloGit {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Victor Cuntsmen");
+    }
+}
