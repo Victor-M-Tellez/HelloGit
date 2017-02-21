d2017ef2ddbb85f75ec2576443d145a25775f6ec
VictorTellez219 <vyctortellez@gmail.com>
2017-02-21 15:29:08 -0500

    Bryant <3's Jessgit statusgit statusgit status

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -4,6 +4,6 @@
 public class HelloGit {
     public static void main(String[] args)
     {
-        System.out.println("Hello Victor Cuntsmen");
+        System.out.println("Hello Bryant Cuntsmen");
     }
 }
